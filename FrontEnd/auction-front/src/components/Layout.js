0ee92f4abc6f1ebe9c25db35217387f87b149598
WonJin <iamwon942@gmail.com>
2024-11-04 17:46:17 +0900

    mainPage

---
+++ FrontEnd/auction-front/src/components/Layout.js
@@ -0,0 +1,16 @@
+import {Outlet} from "react-router-dom";
+import Nav from "./navagation/NavigationBar";
+
+
+const Layout = () =>{
+
+    return(
+        <>
+            <Nav/>
+            {/*<Outlet/>*/}
+            {/*<Footer/>*/}
+        </>
+    )
+}
+
+export default Layout;

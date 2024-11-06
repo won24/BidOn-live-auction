3352d784b4d63ea55e76bc46e7a9e0d16f9a751d
kimsua <k021113a@naver.com>
2024-11-06 16:27:50 +0900

    Mypage

--- FrontEnd/auction-front/src/pages/mypage/MyProfile.js
+++ FrontEnd/auction-front/src/pages/mypage/MyProfile.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const MyProfile = () => {
     return (
-        <div className="myProfile">
+        <div className="MyPorfile">
             <h2>내 정보</h2>
             <p>이름 : 김수아</p>
             <p>회원 정보는 DB 에 담아 가져옴</p>
--- FrontEnd/auction-front/src/pages/mypage/Mypage.js
+++ FrontEnd/auction-front/src/pages/mypage/Mypage.js
@@ -1,21 +1,21 @@
 // 각 섹션(내 정보, 내 글, 즐겨찾기, 참여한 경매) 을 컴포넌트로 분리
 
 import React from "react";
-import MyProfile from "../mypage/MyProfile";
-import MyFar from "../mypage/MyFar";
-import MyNotice from "../mypage/MyNotice";
+import {NavLink} from "react-router-dom";
+import MyProfile from "./MyProfile";
+import MyNotice from "./MyNotice";
+import MyFar from "./MyFar";
+
 
 const Mypage = () => {
 
     return(
         <div className="Mypage">
-            <h2>MyPage</h2>
-            <div className="sections">
-                <MyProfile/> {/* 내 정보 */}
-                <MyNotice/>  {/* 내 글 */}
-                <MyFar/>     {/* 즐겨찾기 */}
-                {/*<MyAuction /> /!* 참여한 경매 *!/*/}
-            </div>
+            <h2>Mypage</h2>
+            <p>마이페이지가 잘 표시됩니다.</p>
+            <NavLink to={MyProfile}>내 정보</NavLink>
+            <NavLink to={MyNotice}>내 글</NavLink>
+            <NavLink to={MyFar}>즐겨찾기</NavLink>
         </div>
     )
 }

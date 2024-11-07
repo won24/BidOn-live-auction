55ee118baaf6bdb20f2fef8f09796ec41ac5048e
kimsua <k021113a@naver.com>
2024-11-07 16:09:43 +0900

    병합충돌

--- FrontEnd/auction-front/src/components/navagation/NavigationBar.js
+++ FrontEnd/auction-front/src/components/navagation/NavigationBar.js
@@ -6,12 +6,12 @@ const Nav = () =>{
 
     return(
         <div>
-            <NavLink to="/live">라이브</NavLink>
-            <NavLink to="/auction">경매품</NavLink>
-            <NavLink to="/mypage">마이페이지</NavLink>
-            <NavLink to="/requestitem">경매품신청</NavLink>
-            <NavLink to="/customer">고객센터</NavLink>
-            <NavLink to="/login">로그인</NavLink>
+            <NavLink to="/live">라이브  </NavLink>
+            <NavLink to="/auction">경매품  </NavLink>
+            <NavLink to="/mypage">마이페이지  </NavLink>
+            <NavLink to="/requestitem">경매품신청  </NavLink>
+            <NavLink to="/customer">고객센터  </NavLink>
+            <NavLink to="/login">로그인  </NavLink>
         </div>
     )
 }
--- FrontEnd/auction-front/src/pages/mypage/Mypage.js
+++ FrontEnd/auction-front/src/pages/mypage/Mypage.js
@@ -1,21 +1,21 @@
 // 각 섹션(내 정보, 내 글, 즐겨찾기, 참여한 경매) 을 컴포넌트로 분리
 
 import React from "react";
-import {NavLink} from "react-router-dom";
-import MyProfile from "./MyProfile";
-import MyNotice from "./MyNotice";
-import MyFar from "./MyFar";
+
+import myFar from "./MyFar";
+import myProfile from "./MyProfile";
+import myNotice from "./MyNotice";
 
 
 const Mypage = () => {
 
     return(
         <div className="Mypage">
-            <h2>Mypage</h2>
-            <p>마이페이지가 잘 표시됩니다.</p>
-            <NavLink to={MyProfile}>내 정보</NavLink>
-            <NavLink to={MyNotice}>내 글</NavLink>
-            <NavLink to={MyFar}>즐겨찾기</NavLink>
+            <h1>Mypage</h1>
+            <button onClick={myFar}>즐겨찾기</button>
+            <button onClick={myProfile}>내 정보</button>
+            <button onClick={myNotice}>내 글</button>
+            <p>마이페이지 표시 안 됩니다 ㅠ</p>
         </div>
     )
 }

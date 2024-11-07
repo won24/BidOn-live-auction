26ab804ee7fbf105d37e826a1651a6a3125806d8
kimsua <k021113a@naver.com>
2024-11-07 18:14:59 +0900

    Mypage

--- FrontEnd/auction-front/src/components/Layout.js
+++ FrontEnd/auction-front/src/components/Layout.js
@@ -1,12 +1,14 @@
 
 import Nav from "./navagation/NavigationBar";
 import Footer from "./footer/Footer";
+import {Outlet} from "react-router-dom";
 
 const Layout = () =>{
     return(
         <>
             <Nav/>
             <Outlet/>
+
             <Footer/>
         </>
     )
--- FrontEnd/auction-front/src/pages/mypage/Mypage.js
+++ FrontEnd/auction-front/src/pages/mypage/Mypage.js
@@ -15,7 +15,7 @@ const Mypage = () => {
             <button onClick={myFar}>즐겨찾기</button>
             <button onClick={myProfile}>내 정보</button>
             <button onClick={myNotice}>내 글</button>
-            <p>마이페이지 표시 안 됩니다 ㅠ</p>
+            <p>마이페이지만 표시 됩니다 ^_^</p>
         </div>
     )
 }

1686d7df179a005186f330d59f7023c5c7e4f54e
kimsua <k021113a@naver.com>
2024-11-15 12:35:29 +0900

    MypageUpdate

--- FrontEnd/auction-front/src/pages/mypage/MyProfile.js
+++ FrontEnd/auction-front/src/pages/mypage/MyProfile.js
@@ -5,13 +5,13 @@
 *
 * 비밀번호는 직접 표시하지 않고 '******' 로 숨겨 보안 처리 함
 *
-* page-container 와 content-container 는 CSS 로 전체 페이지, 내용 중앙 정렬함
+* page-container 와 content-container 는 CSS 로 전체 페이지, 내용 중앙 정렬함 -> 우선 CSS 제외
 * */
 
 import React from "react";
 import {useState} from "react";
 
-import './MyProfile.css';
+// import './MyProfile.css';
 
 const MyProfile = () => {
 

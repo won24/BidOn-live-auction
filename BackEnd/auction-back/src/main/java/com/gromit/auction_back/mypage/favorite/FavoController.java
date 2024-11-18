480a48d1a85379105224cfc13250cc818916f107
kimsua <k021113a@naver.com>
2024-11-18 18:07:31 +0900

    즐겨찾기,,헤헤

--- BackEnd/auction-back/src/main/java/com/gromit/auction_back/mypage/favorite/FavoController.java
+++ BackEnd/auction-back/src/main/java/com/gromit/auction_back/mypage/favorite/FavoController.java
@@ -22,7 +22,10 @@ public class FavoController {
     @PostMapping("/favoList")
     public List<FavoDTO> favolist(@RequestBody FavoDTO favoDTO) {
         System.out.println("요청 받은 데이터: " + favoDTO); // 요청 데이터 확인용 출력
+         int usercode = favoDTO.getUserCode();
+        System.out.println(usercode+"코드");
         List<FavoDTO> allList = favoService.allselect(favoDTO); // 서비스에서 데이터 가져오기
+        System.out.println(allList);
         return allList; // 가져온 데이터를 클라이언트로 반환
     }
 }
--- BackEnd/auction-back/src/main/java/com/gromit/auction_back/mypage/favorite/FavoDTO.java
+++ BackEnd/auction-back/src/main/java/com/gromit/auction_back/mypage/favorite/FavoDTO.java
@@ -9,5 +9,9 @@ import lombok.NoArgsConstructor;
 public class FavoDTO {
     private int userCode;
     private int postId;
-    // 이렇게 하면 게터세터 안 해도 됨
+// 이렇게 하면 게터세터 안 해도 됨
+// 화면에 뿌려주는 거 닉네임,제목, 사진
+
+    private String title;
+    private String imageUrl;
 }

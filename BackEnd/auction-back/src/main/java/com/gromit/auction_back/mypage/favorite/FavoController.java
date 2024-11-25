f13f6d9eb45aa85b635a44f0ca8ec324e2786beb
kimsua <k021113a@naver.com>
2024-11-25 10:07:32 +0900

    CSS start

--- BackEnd/auction-back/src/main/java/com/gromit/auction_back/mypage/favorite/FavoController.java
+++ BackEnd/auction-back/src/main/java/com/gromit/auction_back/mypage/favorite/FavoController.java
@@ -4,6 +4,7 @@ import com.gromit.auction_back.auction.AuctionDTO;
 import com.gromit.auction_back.requestItem.RequestItemDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URLDecoder;
@@ -86,6 +87,4 @@ public class FavoController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
-
-
 }

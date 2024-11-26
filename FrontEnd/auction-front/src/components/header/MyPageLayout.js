e9171e65f46867980c8d860d1ccdf9c73d5c4232
RJANG <msgpigeon@gmail.com>
2024-11-26 17:58:30 +0900

    아 왜 전체화면에선 스크롤이 없고 창에서는 스크롤이 생기지
    다 반응형으로 짜야 하나

--- FrontEnd/auction-front/src/components/header/MyPageLayout.js
+++ FrontEnd/auction-front/src/components/header/MyPageLayout.js
@@ -4,6 +4,7 @@ import MyFar from "../../pages/mypage/MyFar";
 import MyProfile from "../../pages/mypage/MyProfile";
 import MyAuctionItem from "../../pages/mypage/MyAuctionItem";
 import MyAuction from "../../pages/mypage/MyAuction";
+import ChangePassword from "../../pages/mypage/ChangePassword";
 
 
 const MyPageLayout = () => {
@@ -20,7 +21,7 @@ const MyPageLayout = () => {
                 <nav>
                     <NavLink to="/mypage/myfar" onClick={() => handleLinkClick(true)}>| 즐겨찾기 |</NavLink>
                     <NavLink to="/mypage/myprofile" onClick={() => handleLinkClick(true)}> 내 정보 |</NavLink>
-                    {/* <NavLink to="/mypage/changepassword" onClick={() => handleLinkClick(true)}> 비밀번호 변경 |</NavLink> */}
+                    <NavLink to="/mypage/changepassword" onClick={() => handleLinkClick(true)}> 비밀번호 변경 |</NavLink>
                     <NavLink to="/mypage/myauctionitem" onClick={() => handleLinkClick(true)}> 경매품 |</NavLink>
                     <NavLink to="/mypage/myauction" onClick={() => handleLinkClick(true)}> 참여 경매 목록 |</NavLink>
                 </nav>
@@ -29,6 +30,7 @@ const MyPageLayout = () => {
             <Routes>
                 <Route path="myfar" element={<MyFar />}/>
                 <Route path="myprofile" element={<MyProfile />}/>
+                <Route path="changepassword" element={<ChangePassword />}/>
                 <Route path="myauctionitem" element={<MyAuctionItem />} />
                 <Route path="myauction" element={<MyAuction />} />
             </Routes>

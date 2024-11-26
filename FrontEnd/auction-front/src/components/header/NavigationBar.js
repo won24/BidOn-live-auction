b5370046acc3d869e1f0417020eec8bfff97ef2b
RJANG <msgpigeon@gmail.com>
2024-11-26 15:51:03 +0900

    새로고침 부활
    새로고침 부활

--- FrontEnd/auction-front/src/components/header/NavigationBar.js
+++ FrontEnd/auction-front/src/components/header/NavigationBar.js
@@ -1,53 +1,53 @@
-import {NavLink, useLocation, useNavigate} from "react-router-dom";
-import '../../css/NavigationBar.css'
-import {useEffect} from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import "../../css/NavigationBar.css";
 import { useLogin } from "../../pages/login/LoginContext";
 
-const Nav = () =>
+const Nav = () => 
 {
-    const id = sessionStorage.getItem("id");
-    const isLoggedIn = sessionStorage.getItem("isLoggedIn") === "true";
-
     const navigate = useNavigate();
     const location = useLocation();
     const current = location.pathname;
 
-    const { user, setUser, fetchUserData } = useLogin();
+    const isLoggedIn = sessionStorage.getItem("isLoggedIn") === "true";
+    const isAdmin = sessionStorage.getItem("isAdmin") === "true";
+    const cash = sessionStorage.getItem("cash");
+    const nickname = sessionStorage.getItem("nickname");
+    const id = sessionStorage.getItem("id");
 
-    useEffect(() => 
-    {
-        if (isLoggedIn && id) 
-        {
-            fetchUserData(id);
-        }
-    }, [isLoggedIn, id, fetchUserData]);
+    const { user, setUser } = useLogin();
 
     const openCheckoutPopup = () => {
         const url = `${window.location.origin}/checkout`;
         const popupFeatures = "width=600,height=600,scrollbars=yes,resizable=yes";
         window.open(url, "결제하기", popupFeatures);
     };
-    
-    // 충전된 캐시 새로고침(전체 새로고침 아님)
-    // const refresh = () => 
-    // {
-    //     if (id) 
-    //     {
-    //         fetchUserData(id);
-    //     }
-    // };
 
     const handleLogout = () => 
     {
-        if(window.confirm("로그아웃 하시겠습니까?"))
-        {
-            setUser(null);
-            sessionStorage.clear();
-            navigate(current, { replace: true });
-        }
-        else
-        {
-            return;
+        sessionStorage.clear();
+        setUser(null);
+        navigate(current, { replace: true });
+    };
+
+    const updateCash = async () => 
+    {
+        if (!id) return;
+    
+        try {
+            const response = await fetch(`http://localhost:8080/api/id/${id}`);
+            if (response.ok) 
+            {
+                const userData = await response.json(); // Assuming the API returns the full user object
+                const updatedCash = userData.cash; // Extract the `cash` field
+                sessionStorage.setItem("cash", updatedCash); // Update sessionStorage
+                setUser((prev) => ({ ...prev, cash: updatedCash })); // Update context with only the cash value
+            } 
+            else 
+            {
+                console.error("Failed to fetch updated cash.");
+            }
+        } catch (error) {
+            console.error("Error fetching updated cash:", error);
         }
     };
 
@@ -60,47 +60,67 @@ const Nav = () =>
         return nickname;
     };
 
-    return(
-            <div className="navContainer">
-                <a href="/" className="logo"></a>
-                <div className="menuContainer">
-                    <NavLink to="/live" className="nav-link">라이브</NavLink>
-                    <NavLink to="/auction" className="nav-link">경매품</NavLink>
-                    <NavLink to="/requestitem" className="nav-link">경매품신청</NavLink>
-                    <NavLink to="/customer/faq" className="nav-link">고객센터</NavLink>
-                    {isLoggedIn && user?.isAdmin ?
-                        <NavLink to="/admin" className="nav-link">관리자페이지</NavLink>
-                        : <NavLink to="/mypage/myprofile" className="nav-link">마이페이지</NavLink>
-                    }
-                </div>
-                    {isLoggedIn ? (
-                        <div>
-                            <div style={{ marginBottom: "3px" }}>
-                                <span className="user-welcome">
-                                    {user?.isAdmin ? "[관리자] " : ""}{truncateNickname(user?.nickname, 50)}님, 환영합니다.
-                                </span>
-                                <button className="login-button2" onClick={handleLogout}>로그아웃</button>
-                            </div>
-
-                            <div className="user-info-container">
-                                {!user?.isAdmin ?
-                                    <>
-                                        <span className="user-welcome">
-                                            충전된 캐시: {user?.userCash} 캐시
-                                        </span>
-                                        {/* 코드 고치면서 충전 즉시 반영되게 변경 */}
-                                        {/* <button className="login-button2" onClick={refresh}>새로고침</button> */}
-                                        <button className="login-button2" onClick={openCheckoutPopup}>충전하기</button>
-                                    </>
-                                    :<></>
-                                }
-                            </div>
-                        </div>
+    return (
+        <div className="navContainer">
+            <a href="/" className="logo"></a>
+            <div className="menuContainer">
+                <NavLink to="/live" className="nav-link">
+                    라이브
+                </NavLink>
+                <NavLink to="/auction" className="nav-link">
+                    경매품
+                </NavLink>
+                <NavLink to="/requestitem" className="nav-link">
+                    경매품신청
+                </NavLink>
+                <NavLink to="/customer/faq" className="nav-link">
+                    고객센터
+                </NavLink>
+                {isLoggedIn && isAdmin ? (
+                    <NavLink to="/admin" className="nav-link">
+                        관리자페이지
+                    </NavLink>
                 ) : (
-                    <button className="login-button2" onClick={() => navigate("/member/login")}>로그인</button>
+                    <NavLink to="/mypage/myprofile" className="nav-link">
+                        마이페이지
+                    </NavLink>
                 )}
             </div>
-    )
-}
+            {isLoggedIn ? (
+                <div>
+                    <div style={{ marginBottom: "3px" }}>
+                        <span className="user-welcome">
+                            {isAdmin ? "[관리자] " : ""}
+                            {truncateNickname(nickname, 50)}님, 환영합니다.
+                        </span>
+                        <button className="login-button2" onClick={handleLogout}>
+                            로그아웃
+                        </button>
+                    </div>
+                    {!isAdmin && (
+                        <div className="user-info-container">
+                            <span className="user-welcome">
+                                충전된 캐시: {user?.cash || cash} 캐시
+                            </span>
+                            <button className="login-button2" onClick={updateCash}>
+                                새로고침
+                            </button>
+                            <button className="login-button2" onClick={openCheckoutPopup}>
+                                충전하기
+                            </button>
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <button
+                    className="login-button2"
+                    onClick={() => navigate("/member/login")}
+                >
+                    로그인
+                </button>
+            )}
+        </div>
+    );
+};
 
 export default Nav;
--- FrontEnd/auction-front/src/pages/mypage/ChangePassword.js
+++ FrontEnd/auction-front/src/pages/mypage/ChangePassword.js
@@ -1,251 +1,136 @@
-// import { useEffect, useState } from "react";
-// import NameInput from "../signup/formFields/NameInput";
-// import PhoneInput from "../signup/formFields/PhoneInput";
-// import EmailInput from "../signup/formFields/EmailInput";
-// import IdInput from "../signup/formFields/IdInput";
-// import { useLocation } from "react-router-dom";
-// import ConfirmPasswordInput from "../signup/formFields/ConfirmPasswordInput";
-// import PasswordInput from "../signup/formFields/PasswordInput";
+import { useEffect, useState } from "react";
+import ConfirmPasswordInput from "../signup/formFields/ConfirmPasswordInput";
+import PasswordInput from "../signup/formFields/PasswordInput";
 
-// // Utility functions for routes
-// const getRouteConfig = (formData, setFormData, handleChange) => (
-// {
-//     "/changeer/pw/phone": 
-//     {
-//         inputs: [
-//             <IdInput value={formData.id} onChange={handleChange} validate={false} />,
-//             <NameInput value={formData.name} onChange={handleChange} />,
-//             <PhoneInput phone={formData.phone} setFormData={setFormData} formData={formData} />,
-//         ],
-//         validate: () =>
-//             formData.id.trim() &&
-//             formData.name.trim() &&
-//             formData.phone.areaCode.trim() &&
-//             formData.phone.middle.trim() &&
-//             formData.phone.last.trim(),
-//         getRelevantData: () => (
-//         {
-//             id: formData.id,
-//             name: formData.name,
-//             phone: `${formData.phone.areaCode}-${formData.phone.middle}-${formData.phone.last}`,
-//         }),
-//     },
+const ResultMessage = ({ message, formData, handleChange, handlePasswordSubmit, passwordChanged }) => 
+{
+    return(
+        <div className="change-pw-result">
+            <h3 className="subtitle">비밀번호 변경</h3>
+            <hr className="line" />
+            { passwordChanged ? 
+            (<>
+                <span className="message">닫기 버튼을 눌러 창을 닫아주세요.</span>
+                <div style={{marginTop: "50px"}}>
+                    <h1 style={{ textAlign: "center", marginBottom: "60px" }}>{message}</h1>
+                    <div className="button-wrapper">
+                        <button className="auth-link">
+                            닫기
+                        </button>
+                    </div>
+                </div>
+            </>) : 
+            (<>
+                <span className="message">{message}</span>
+                <div>
+                    <form onSubmit={handlePasswordSubmit}>
+                        <PasswordInput value={formData.password} onChange={handleChange} />
+                        <ConfirmPasswordInput
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
+                            password={formData.password}
+                        />
+                        <div className="button-wrapper">
+                            <button type="submit" className="auth-link">
+                                변경하기
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </>) }
+        </div>
+    )
+};
 
-//     "/changeer/pw/email": 
-//     {
-//         inputs: [
-//             <IdInput value={formData.id} onChange={handleChange} validate={false} />,
-//             <NameInput value={formData.name} onChange={handleChange} />,
-//             <EmailInput value={formData.email} onChange={handleChange} />,
-//         ],
-//         validate: () => formData.id.trim() && formData.name.trim() && formData.email.trim(),
-//         getRelevantData: () => (
-//         {
-//             id: formData.id,
-//             name: formData.name,
-//             email: formData.email,
-//         }),
-//     },
-// });
+const ChangePassword = () => 
+{
+    const [formData, setFormData] = useState(
+    {
+        password: '',
+        newPassword: '',
+        confirmNewPassword: '',
+    });
+    const [success, setSuccess] = useState(false);
+    const [fail, setFail] = useState(false);
+    const [formValid, setFormValid] = useState(false);
+    const [resultMessage, setResultMessage] = useState("");
+    const [passwordChanged, setPasswordChanged] = useState(false);
 
-// const ResultMessage = ({ message, onClose, formData, handleChange, handlePasswordSubmit, passwordChanged }) => 
-// {
-//     return(
-//         <div className="change-pw-result">
-//             <h3 className="subtitle">비밀번호 변경</h3>
-//             <hr className="line" />
-//             { passwordChanged ? 
-//             (<>
-//                 <span className="message">닫기 버튼을 눌러 창을 닫아주세요.</span>
-//                 <div style={{marginTop: "50px"}}>
-//                     <h1 style={{ textAlign: "center", marginBottom: "60px" }}>{message}</h1>
-//                     <div className="button-wrapper">
-//                         <button className="auth-link" onClick={onClose}>
-//                             닫기
-//                         </button>
-//                     </div>
-//                 </div>
-//             </>) : 
-//             (<>
-//                 <span className="message">{message}</span>
-//                 <div>
-//                     <form onSubmit={handlePasswordSubmit}>
-//                         <PasswordInput value={formData.password} onChange={handleChange} />
-//                         <ConfirmPasswordInput
-//                             value={formData.confirmPassword}
-//                             onChange={handleChange}
-//                             password={formData.password}
-//                         />
-//                         <div className="button-wrapper">
-//                             <button type="submit" className="auth-link">
-//                                 변경하기
-//                             </button>
-//                         </div>
-//                     </form>
-//                 </div>
-//             </>) }
-//         </div>
-//     )
-// };
+    const handleChange = (e) => 
+    {
+        const { name, value } = e.target;
+        setFormData((prevData) => (
+        {
+            ...prevData,
+            [name]: value,
+        }));
+    };
 
-// const ChangePassword = () => 
-// {
-//     const [formData, setFormData] = useState(
-//     {
-//         password: '',
-//         newPassword: '',
-//         confirmNewPassword: '',
-//     });
-//     const [success, setSuccess] = useState(false);
-//     const [fail, setFail] = useState(false);
-//     const [formValid, setFormValid] = useState(false);
-//     const [resultMessage, setResultMessage] = useState("");
-//     const { pathname } = useLocation();
-//     const [passwordChanged, setPasswordChanged] = useState(false);
+    const handlePasswordSubmit = async (e) => 
+    {
+        e.preventDefault();
 
-//     const handleChange = (e) => 
-//     {
-//         const { name, value } = e.target;
-//         setFormData((prevData) => (
-//         {
-//             ...prevData,
-//             [name]: value,
-//         }));
-//     };
+        if (formData.password !== formData.confirmNewPassword) 
+        {
+            alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+            return;
+        }
 
-//     const close = () => 
-//     {
-//         window.close();
-//     };
+        const dataToSend = 
+        {
+            id: formData.id,
+            password: formData.password,
+        };
 
-//     const routeConfig = getRouteConfig(formData, setFormData, handleChange);
-//     const currentRoute = routeConfig[pathname];
+        try {
+            const response = await fetch("http://localhost:8080/api/finder/pw/update", 
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(dataToSend),
+            });
 
-//     useEffect(() => 
-//     {
-//         if (currentRoute) 
-//         {
-//             setFormValid(currentRoute.validate());
-//         } 
-//         else 
-//         {
-//             setFormValid(false);
-//         }
-//     }, [formData, currentRoute]);
+            if (response.ok) 
+            {
+                setResultMessage("비밀번호가 성공적으로 변경되었습니다.");
+                setPasswordChanged(true);
+            } 
+            else 
+            {
+                setResultMessage("비밀번호 변경에 실패했습니다. 다시 시도해주세요.");
+            }
+        } catch (error) 
+        {
+            setResultMessage("오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+        }
+    };
 
-//     const handleSubmit = async (e) => 
-//     {
-//         e.preventDefault();
-//         const dataToSend = currentRoute.getRelevantData();
-//         const queryParams = new URLSearchParams(dataToSend);
+    if (success || fail) 
+    {
+        return <ResultMessage 
+                    message={resultMessage} 
+                    formData={formData} 
+                    handleChange={handleChange}
+                    handlePasswordSubmit={handlePasswordSubmit}
+                    passwordChanged={passwordChanged}
+                />;
+    }
 
-//         try {
-//             const response = await fetch(
-//                 `http://localhost:8080/api/finder/pw?${queryParams.toString()}`,
-//                 {
-//                     method: "GET",
-//                     headers: { "Content-Type": "application/json" },
-//                 }
-//             );
+    return (
+        <div>
+            <form>
+                <div className="button-wrapper">
+                    <button
+                        type="submit"
+                        className={`auth-link ${formValid ? "" : "disabled"}`}
+                        disabled={!formValid}
+                    >
+                        조회하기
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
 
-//             const result = await response.json();
-
-//             if (result) 
-//             {
-//                 setSuccess(true);
-//                 setResultMessage("회원정보 조회에 성공하여 비밀번호를 재설정합니다.");
-//             } 
-//             else if (response.status === 404) 
-//             {
-//                 setFail(true);
-//                 setResultMessage("일치하는 정보를 찾을 수 없습니다.");
-//             }
-//         } catch {
-//             setResultMessage("잠시 후 다시 시도해주세요.");
-//         }
-//     };
-
-//     const handlePasswordSubmit = async (e) => 
-//     {
-//         e.preventDefault();
-
-//         if (formData.password !== formData.confirmPassword) 
-//         {
-//             alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
-//             return;
-//         }
-
-//         const dataToSend = 
-//         {
-//             id: formData.id,
-//             password: formData.password,
-//         };
-
-//         try {
-//             const response = await fetch("http://localhost:8080/api/finder/pw/update", 
-//             {
-//                 method: "POST",
-//                 headers: { "Content-Type": "application/json" },
-//                 body: JSON.stringify(dataToSend),
-//             });
-
-//             if (response.ok) 
-//             {
-//                 setResultMessage("비밀번호가 성공적으로 변경되었습니다.");
-//                 setPasswordChanged(true);
-//             } 
-//             else 
-//             {
-//                 setResultMessage("비밀번호 변경에 실패했습니다. 다시 시도해주세요.");
-//             }
-//         } catch (error) 
-//         {
-//             setResultMessage("오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
-//         }
-//     };
-
-//     if (success || fail) 
-//     {
-//         return <ResultMessage 
-//                     message={resultMessage} 
-//                     onClose={close} 
-//                     formData={formData} 
-//                     handleChange={handleChange}
-//                     handlePasswordSubmit={handlePasswordSubmit}
-//                     passwordChanged={passwordChanged}
-//                 />;
-//     }
-
-//     if (!currentRoute) 
-//     {
-//         return (
-//             <div>
-//                 <h1 style={{ textAlign: "center" }}>잘못된 접근입니다.</h1>
-//                 <div className="button-wrapper">
-//                     <button className="auth-link" onClick={close}>
-//                         닫기
-//                     </button>
-//                 </div>
-//             </div>
-//         );
-//     }
-
-//     return (
-//         <div>
-//             <form onSubmit={handleSubmit}>
-//                 {currentRoute.inputs}
-//                 <div className="button-wrapper">
-//                     <button
-//                         type="submit"
-//                         className={`auth-link ${formValid ? "" : "disabled"}`}
-//                         disabled={!formValid}
-//                     >
-//                         조회하기
-//                     </button>
-//                 </div>
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default ChangePassword;
+export default ChangePassword;
